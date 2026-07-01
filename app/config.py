724d5696b79aae78f332e3fdedaf6b9836227f0c
krish <krishnasaibairi56140@gmail.com>
2026-07-01 12:54:42 +0530

    Basic configuartion done

---
+++ app/config.py
@@ -0,0 +1,2 @@
+APPLICATION_NAME = "enterprise-ai-assistant"
+APPLICATION_VERSION = "0.1.0"
--- app/main.py
+++ app/main.py
@@ -1,7 +1,8 @@
 from fastapi import FastAPI
+from app.config import APPLICATION_NAME, APPLICATION_VERSION
 
 app = FastAPI()
 
 @app.get("/health")
 def health_check() -> dict:
-    return {"status": "healthy"}
+    return {"status": "healthy", "application": APPLICATION_NAME, "application_version": APPLICATION_VERSION}
