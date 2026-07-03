ee4597157718b5a49286d9d5e85b26598f1e1b04
krish <krishnasaibairi56140@gmail.com>
2026-07-03 20:25:45 +0530

    Response model added

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -1,12 +1,12 @@
 from fastapi import APIRouter
 
 from app.models.requests import RegisterRequest
+from app.models.responses import RegisterResponse
 from app.services.auth_service import AuthService
 
 router = APIRouter()
 auth_service = AuthService()
 
-@router.post("/register")
-def test_register_request(request: RegisterRequest):
-    response = auth_service.register_user()
-    return response
+@router.post("/register", response_model=RegisterResponse)
+def test_register_request(request: RegisterRequest): 
+    return auth_service.register_user()
--- app/services/auth_service.py
+++ app/services/auth_service.py
@@ -1,5 +1,8 @@
+
+from app.models.responses import RegisterResponse
+
+
 class AuthService:
+
     def register_user(self):
-        return {
-            "message": "Registration service called successfully"
-        }
+        return RegisterResponse(message="Registration service called successfully")
