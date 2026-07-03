f366f2b72d87ed4ca3f3b74aa59bfd08e181aaa2
krish <krishnasaibairi56140@gmail.com>
2026-07-03 20:00:47 +0530

    Service layer added

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -1,9 +1,12 @@
 from fastapi import APIRouter
 
 from app.models.requests import RegisterRequest
+from app.services.auth_service import AuthService
 
 router = APIRouter()
+auth_service = AuthService()
 
 @router.post("/register")
 def test_register_request(request: RegisterRequest):
-    print(f"Received registration request: {request}")
+    response = auth_service.register_user()
+    return response
---
+++ app/services/auth_service.py
@@ -0,0 +1,5 @@
+class AuthService:
+    def register_user(self):
+        return {
+            "message": "Registration service called successfully"
+        }
